e73da784aeeb86336b217ba1e5cb582b266aa02b
rsulkowicz <radinho@mail.com>
2017-01-22 18:02:54 +0100

    Zmiany kosmetyczne

--- html/views/kategoria/kategoria_index.php
+++ html/views/kategoria/kategoria_index.php
@@ -3,19 +3,18 @@
 <div class="table-responsive">
  <table class="table table-bordered table-hover">
  <tr>
- <th>Id</th><th>Nazwa</th><th>Edytuj</th><th>Usuń</th>
+ <th>Nazwa</th><th>Edytuj</th><th>Usuń</th>
  </tr>
  <?php
  foreach ($results as $row) {
  echo '<tr>';
- echo '<td>' . $row['id_kategorii'] . '</td>';
  echo '<td>' . $row['nazwa'] . '</td>';
- echo '<td><a href="kategoria/edit/' . $row['id_kategorii'] . '">Edytuj</a></td>';
- echo '<td><a href="kategoria/delete/' . $row['id_kategorii'] . '">Usuń</a></td>';
+ echo '<td><a href="kategoria/edit/' . $row['id_kategoria'] . '">Edytuj</a></td>';
+ echo '<td><a href="kategoria/delete/' . $row['id_kategoria'] . '">Usuń</a></td>';
  echo '</tr>';
  }
  ?>
  </table>
 
 </div>
-<a href="kategoria/add">Dodaj</a>
+<a href="kategoria/add"><button class="btn btn-default" type="submit">Dodaj</button></a>
