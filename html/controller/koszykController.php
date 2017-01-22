461db39ce619246ea15f0cf91e8adb1d2a4e4d39
rsulkowicz <radinho@mail.com>
2017-01-22 16:58:37 +0100

    Uwzględnienie zakupionych oraz autorów

--- html/controller/koszykController.php
+++ html/controller/koszykController.php
@@ -1,75 +1,74 @@
 <?php
 class koszykController extends baseController {
- //Zawartość koszyka
- public function index() {
- $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
- $produkty = array();
- $calk_ilosc = 0;
- $wart = 0;
- if (isset($_SESSION['koszyk']) && isset($_SESSION['calkowita_ilosc']) && isset($_SESSION['wartosc'])) {
- $db = $this->registry->db;
- foreach ($_SESSION['koszyk'] as $idProduktu => $ilosc) {
- $produkt = $db::getProduktById($idProduktu);
- $wart = $_SESSION['wartosc'];
- $calk_ilosc = $_SESSION['calkowita_ilosc'];
- $produkty[] = $produkt;
- }
- }
- $this->registry->template->produkty = $produkty;
- $this->registry->template->calkowita_ilosc = $calk_ilosc;
- $this->registry->template->wartosc = $wart;
- $this->registry->template->show('koszyk/koszyk_index');
- }
+    //Zawartość koszyka
+    public function index() {
+        $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
+        $pozycje = array();
+        $calk_ilosc = 0;
+        $wart = 0;
+        if (isset($_SESSION['koszyk']) && isset($_SESSION['calkowita_ilosc']) && isset($_SESSION['wartosc'])) {
+            $db = $this->registry->db;
+            foreach ($_SESSION['koszyk'] as $idPozycja => $ilosc) {
+                $pozycja = $db::getPozycjaById($idPozycja);
+                $wart = $_SESSION['wartosc'];
+                $calk_ilosc = $_SESSION['calkowita_ilosc'];
+                $pozycje[] = $pozycja;
+            }
+        }
+        $this->registry->template->pozycje = $pozycje;
+        $this->registry->template->calkowita_ilosc = $calk_ilosc;
+        $this->registry->template->wartosc = $wart;
+        $this->registry->template->show('koszyk/koszyk_index');
+    }
+    //Odświeżenie zawartości
+    private function refreshShoppingCart() {
+        $calkowita_ilosc = 0;
+        $wartosc = 0;
+        if (isset($_SESSION['koszyk']) && isset($_SESSION['calkowita_ilosc']) && isset($_SESSION['wartosc'])) {
+            $db = $this->registry->db;
+            foreach ($_SESSION['koszyk'] as $idPozycja => $ilosc) {
+                $pozycja = $db::getPozycjaById($idPozycja);
+                $calkowita_ilosc ++;
+                $wartosc += $pozycja->getCena();
+            }
+        }
+        $_SESSION['calkowita_ilosc'] = $calkowita_ilosc;
+        $_SESSION['wartosc'] = $wartosc;
+    }
 
- //Odświeżenie zawartości
- private function refreshShoppingCart() {
- $calkowita_ilosc = 0;
- $wartosc = 0;
- if (isset($_SESSION['koszyk']) && isset($_SESSION['calkowita_ilosc']) && isset($_SESSION['wartosc'])) {
- $db = $this->registry->db;
- foreach ($_SESSION['koszyk'] as $idProduktu => $ilosc) {
- $produkt = $db::getProduktById($idProduktu);
- $calkowita_ilosc += $ilosc;
- $wartosc += $ilosc * $produkt->getCena();
- }
- }
- $_SESSION['calkowita_ilosc'] = $calkowita_ilosc;
- $_SESSION['wartosc'] = $wartosc;
- }
+    //Dodanie pozycji
+    public function add() {
+        $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
+        $idPozycja = $this->registry->id;
+        if (!isset($_SESSION['koszyk']) || !isset($_SESSION['calkowita_ilosc']) || !isset($_SESSION['wartosc'])) {
+            $_SESSION['koszyk'] = array();
+            $_SESSION['calkowita_ilosc'] = 0;
+            $_SESSION['wartosc'] = 0;
+        }
+        if (isset($_SESSION['koszyk'][$idPozycja])) {
+            $_SESSION['koszyk'][$idPozycja] ++;
+        } else {
+            $_SESSION['koszyk'][$idPozycja] = 1;
+        }
+        $this->refreshShoppingCart();
+        $location = '/' . APP_ROOT . '/koszyk';
+        header("Location: $location");
+    }
 
- //Dodanie pozycji
- public function add() {
- $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
- $idProduktu = $this->registry->id;
- if (!isset($_SESSION['koszyk']) || !isset($_SESSION['calkowita_ilosc']) || !isset($_SESSION['wartosc'])) {
- $_SESSION['koszyk'] = array();
- $_SESSION['calkowita_ilosc'] = 0;
- $_SESSION['wartosc'] = 0;
- }
- if (isset($_SESSION['koszyk'][$idProduktu])) {
- $_SESSION['koszyk'][$idProduktu] ++;
- } else {
- $_SESSION['koszyk'][$idProduktu] = 1;
- }
- $this->refreshShoppingCart();
- $location = '/' . APP_ROOT . '/koszyk';
- header("Location: $location");
- }
-
- //Edycja zawartości
- public function edit() {
- $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
- if (isset($_SESSION['koszyk'])) {
- foreach ($_SESSION['koszyk'] as $idProduktu => $ilosc) {
- if (isset($_POST[$idProduktu])) {
- var_dump($_POST);
- $nowaIlosc = $_POST[$idProduktu];
- if ($nowaIlosc < 0) {
- continue;}
- if ($nowaIlosc == 0) {
- unset($_SESSION['koszyk'][$idProduktu]);
- } else {
- $_SESSION['koszyk'][$idProduktu] = $nowaIlosc;
+    //Edycja zawartości
+    public function edit() {
+        $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
+        if (isset($_SESSION['koszyk'])) {
+            foreach ($_SESSION['koszyk'] as $idPozycja => $ilosc) {
+                if (isset($_POST[$idPozycja])) {
+                    var_dump($_POST);
+                    $nowaIlosc = $_POST[$idPozycja];
+                    if ($nowaIlosc < 0) {
+                        continue;}
+                    if ($nowaIlosc == 0) {
+                        unset($_SESSION['koszyk'][$idPozycja]);
+                    } else {
+                        $_SESSION['koszyk'][$idPozycja] = $nowaIlosc;
  }
  }
  }
@@ -82,10 +81,10 @@ class koszykController extends baseController {
  //Usunięcie pozycji
  public function delete() {
  $this->ograniczDostepTylkoDlaZalogowanegoUzytkownika();
- $idProduktu = $this->registry->id;
+ $idPozycja = $this->registry->id;
 
- if (isset($_SESSION['koszyk'][$idProduktu])) {
- unset($_SESSION['koszyk'][$idProduktu]);
+ if (isset($_SESSION['koszyk'][$idPozycja])) {
+ unset($_SESSION['koszyk'][$idPozycja]);
  $this->refreshShoppingCart();
  }
  $location = '/' . APP_ROOT . '/koszyk';
