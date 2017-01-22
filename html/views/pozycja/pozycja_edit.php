b7d04eaeb3a6f997a355ce449bd2f2d6201eaa3a
rsulkowicz <radinho@mail.com>
2017-01-22 18:07:19 +0100

    Zmiany związane z dodaniem autorów

--- html/views/pozycja/pozycja_edit.php
+++ html/views/pozycja/pozycja_edit.php
@@ -1,6 +1,8 @@
 <h1>Edytuj pozycję</h1>
 <?php
 $nazwa = "";
+$autorzy = array();
+$rok_wydania = "";
 $cena = "";
 $opis = "";
 $kategorie = array();
@@ -8,7 +10,8 @@ $id = "";
 
 if (!empty($model)) {
     $id = $model->getIdPozycja();
-    $nazwa = $model->getNazwa();
+    $nazwa = $model->getTytuł();
+    $autorPozycji = $model->getAutor();
     $cena = $model->getCena();
     $opis = $model->getOpis();
     $kategoriaPozycji = $model->getKategoria();
@@ -17,9 +20,27 @@ if (!empty($model)) {
 
 <form method="POST" action="/<?= APP_ROOT ?>/pozycja/edit">
     <div class="form-group">
-        <label>Nazwa </label>
-        <input class="form-control" type="text" name="nazwa" value="<?= $nazwa ?>" /> 
+        <label>Tytuł </label>
+        <input class="form-control" type="text" name="tytuł" value="<?= $nazwa ?>" /> 
         <input type="hidden" name="id" value="<?= $id ?>" />
+    </div>
+    <div class="form-group">
+        <label>Autor </label>
+        <select name="autor" class="form-control">
+            <?php
+            foreach ($autorzyAll as $autor) {
+                if ($autor['imie_nazwisko'] == $autorPozycji->getIdAutor()) {
+                    echo '<option value="' . $autor['id_autor'] . '" selected>' . $autor['imie_nazwisko'] . '</option>';
+                } else {
+                    echo '<option value="' . $autor['id_autor'] . '" >' . $autor['imie_nazwisko'] . '</option>';
+                }
+            }
+            ?>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Rok wydania </label>
+        <input class="form-control" type="text" name="rok_wydania" value="<?= $rok_wydania ?>"/>
     </div>
     <div class="form-group">
         <label>Cena </label>
@@ -30,7 +51,7 @@ if (!empty($model)) {
         <select name="kategoria" class="form-control">
             <?php
             foreach ($kategorieAll as $kategoria) {
-                if ($kategoria['id_kategoria'] == $kategoriaPozycji->getIdKategorii()) {
+                if ($kategoria['id_kategoria'] == $kategoriaPozycji->getIdKategoria()) {
                     echo '<option value="' . $kategoria['id_kategoria'] . '" selected>' . $kategoria['nazwa'] . '</option>';
                 } else {
                     echo '<option value="' . $kategoria['id_kategoria'] . '" >' . $kategoria['nazwa'] . '</option>';
